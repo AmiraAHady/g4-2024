8c7a3db5e57f5e4c1b34227515eb4b3abc13bdf9
AmiraAHady <amirasgithub@gmail.com>
2024-09-15 12:33:35 +0300

    day 6'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MoviesComponent } from './components/movies/movies.component';
+// Decorator
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // dependencies
+  imports: [NavBarComponent, FooterComponent, MoviesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'myApp';
-}
+export class AppComponent {}
